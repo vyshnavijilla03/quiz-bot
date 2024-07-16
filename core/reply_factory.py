6a1ad58b1613e21e2cc5bc0e8a5aa94bc9ec8b84
vyshnavi jilla <116245749+vyshnavijilla03@users.noreply.github.com>
2024-07-16 15:32:50 +0530

    Update reply_factory.py

--- core/reply_factory.py
+++ core/reply_factory.py
@@ -29,7 +29,12 @@ def generate_bot_responses(message, session):
 
 
 def record_current_answer(answer, current_question_id, session):
-    '''
+    correct_answer=PYTHON_QUESTION_LIST[correct_question_id]['answer']
+    is_correct=answer.lower().strip()==correct_answer.lower().strrip()
+    session["answers"][current_question_id]={
+        "answer":answer,
+        "is_corrct": is_corect
+        '''
     Validates and stores the answer for the current question to django session.
     '''
     return True, ""
@@ -39,6 +44,12 @@ def get_next_question(current_question_id):
     '''
     Fetches the next question from the PYTHON_QUESTION_LIST based on the current_question_id.
     '''
+    next_question_id=current_question_id+1
+    if next_question_id<len(PYTHON_QUESTION_LIST):
+        next_question=PYTHON_QUESTION_LIST[next_question_id]['question']
+        return next_question,next_question_id
+    else:
+        return None, None
 
     return "dummy question", -1
 
@@ -48,5 +59,9 @@ def generate_final_response(session):
     Creates a final result message including a score based on the answers
     by the user for questions in the PYTHON_QUESTION_LIST.
     '''
+    answers=session.get("answers",{})
+    total_questions=len(PYTHON_QUESTION_LIST)
+    correct_answers=sum(1 for answer in answers.values() if answer['is_scorrect'])
+    return f"You have completed the quiz! your score is {correct_anwers} out of {total_questions}."
 
     return "dummy result"
